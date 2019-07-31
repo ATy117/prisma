1fc509129f874994e74b39aeb46bef47ea6f3b56
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-07-31 20:16:07 +0800

    populate works for the mean time

--- models/Account.js
+++ models/Account.js
@@ -6,11 +6,73 @@ const accountSchema = new Schema({
     password: String,
     firstname: String,
     lastname: String,
+    followed: [{
+        type: Schema.Types.ObjectId,
+        ref: "Account"
+    }],
     followers: [{
         type: Schema.Types.ObjectId,
         ref: "Account"
     }]
 });
+
+// STATICS -----------------------------------------------------
+// Use for checking if username exists
+accountSchema.statics.checkUniqueUsername = function(usernameTest, callback){
+    this.findOne({
+        username : usernameTest
+    }, callback);
+};
+
+// Use for checking if account exists
+accountSchema.statics.checkAccountExists = function(usernameTest, passwordTest, callback){
+    this.findOne({
+        username : usernameTest,
+        password : passwordTest
+    }, callback);
+}
+
+//User for adding accounts, given account object, then return that object
+accountSchema.statics.addAccount = function(account, callback){
+    account.save().then(callback);
+};
+
+//Logging in retrieve of account
+accountSchema.statics.login = function(username, password, callback){
+    this.findOne({
+        username : username,
+        password: password
+    }, callback);
+};
+
+//Retrieve account given ID, navigation purposes
+accountSchema.statics.getAccountByUsername = function (username, callback){
+    this.findOne({
+        username: username
+    }, callback);
+};
+
+
+// METHODS ---------------------------------------------------------------
+//Retrieve followers
+accountSchema.methods.getFollowers = function(callback){
+    Account.findOne({
+        username: this.username
+    }).populate('followers').exec(callback);
+};
+
+//Retrieve followed
+accountSchema.methods.getFollowed = function(callback){
+    Account.findOne({
+        username: this.username
+    }).populate('followed').exec(callback);
+};
+
+// Follow an account
+accountSchema.methods.followAccount = function(followedAccount, callback){
+    this.followers.push(followedAccount._id);
+    this.save().then(callback);
+};
 
 const Account = mongoose.model("Account", accountSchema);
 
