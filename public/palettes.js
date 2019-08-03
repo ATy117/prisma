75db15be409951fd94a82eb64b464337fd772b69
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-08-04 01:55:14 +0800

    logout

--- public/palettes.js
+++ public/palettes.js
@@ -1,27 +1,6 @@
-
-/*window.onload = function(){
-
-    console.log("palette.js");
-
-    $("#delete_button_id").click(async function(e){
-        console.log("Pressed Delete");
-        $("#delete_button_id").remove();
-        $(".edit_button").remove();
-
-        var confirm_button = document.createElement('div');
-        var cancel_button = document.createElement('div');
-        confirm_button.appendChild(document.createTextNode("Confirm"));
-        cancel_button.appendChild(document.createTextNode("Cancel"));
-        confirm_button.classList.add("delete_confirmation_button");
-        cancel_button.classList.add("delete_confirmation_button");
-
-        document.getElementById('icons_container').append(confirm_button);
-        document.getElementById('icons_container').append(cancel_button);
-    })
-}*/
-
-$(document).on("click", "#delete_button_id", function(){
+$(document).on("click", "#generator", function(){
     $(this).remove();
-    $("#edit_button_id").remove();
+    let 
+
 
 })
