dae77232ad1f821457872b98f4ecab7a24288487
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-08-05 12:58:42 +0800

    search

---
+++ public/search.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#follow_button").click(function(){
+        $("#follow_span").text("Unfollow");
+        //change class to -> follow_user_button_clicked
+
+    })
+})
