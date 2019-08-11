ba08cbc4b1196a285f3a90d343d69c8a9d45fd6f
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-08-11 19:59:10 +0800

    search

--- public/feed.js
+++ public/feed.js
@@ -1,4 +1,14 @@
-$(document).on("click", "#like_palette_id", function(){
-    $(this).toggleClass("like_palette_color");
-    $(this).attr("src", "./assets//hearPink.png");
+$(document).ready(function(){
+    $(document).on("click", "#like_palette_id", function(){
+        if($(this).attr("data-liked") == "liked") {
+            $(this).attr("src","/assets/heartGray.png");
+            $(this).attr("data-liked", "unliked");
+            // unlike it here
+        }
+        else {
+            $(this).attr("src","/assets/hearPink.png");
+            $(this).attr("data-liked", "liked");
+            // like it here
+        }
+    });
 })
--- public/search.js
+++ public/search.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
         if (check==0){
             $("#follow_span").text("Unfollow");
             check = 1;
+            this.className = "unfollow_user_button";
             //change class to -> follow_user_button_clicked
         } else{
             $("#follow_span").text("Follow");
             check = 0;
+            this.className = "follow_user_button";
             //change class to -> follow_user_button_clicked
         }
         
