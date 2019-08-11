1cc74c5de3f16f0f7a6337d3db05a53f0c4e9b0e
rebecabecalyn <45415030+rebecabecalyn@users.noreply.github.com>
2019-08-11 20:31:40 +0800

    search

--- public/search.js
+++ public/search.js
@@ -14,4 +14,18 @@ $(document).ready(function(){
         }
         
     })
+
+
+    $(document).on("click", "#like_palette_id", function(){
+        if($(this).attr("data-liked") == "liked") {
+            $(this).attr("src","/assets/heartGray.png");
+            $(this).attr("data-liked", "unliked");
+            // unlike it here
+        }
+        else {
+            $(this).attr("src","/assets/hearPink.png");
+            $(this).attr("data-liked", "liked");
+            // like it here
+        }
+    });
 })
