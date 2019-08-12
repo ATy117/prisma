df05b23348ebedfcb9a64d5b5b9b769eadc9e767
ATy117 <41797980+ATy117@users.noreply.github.com>
2019-08-12 20:25:46 +0800

    profile's liked palettes work

--- routes/profile_route.js
+++ routes/profile_route.js
@@ -18,12 +18,43 @@ router.use(login_checker);
 router.use(account_getter);
 
 // View the profile
-router.get("/", function(req,res){
+router.get("/", async function(req,res){
+
+    let account = req.account;
+    
+    let paletteResults = await account.getLikedPalettes();
+    let palettes = [];
+    let palettesCount = 0;
+    for (let i= 0; i< paletteResults.length; i++){
+        // Palette json object
+        let unpopulatedResult = paletteResults[i];
+
+        let oneResult = await unpopulatedResult.populateCreator();
+
+        //Check if you are following this user
+        // The user info to be rendered
+        let soloPalette = {
+            "id" : oneResult._id,
+            "name" : oneResult.name,
+            "firstname": oneResult.creator.firstname,
+            "lastname" : oneResult.creator.lastname,
+            "dateCreated" : oneResult.dateCreated,
+            "color1": oneResult.color1,
+            "color2": oneResult.color2,
+            "color3": oneResult.color3,
+            "color4": oneResult.color4,
+            "color5": oneResult.color5
+        };
+        palettes.push(soloPalette);
+        palettesCount++;
+    }
     // render the profile
     res.render("myprofile.hbs", {
         username: req.account.username,
         firstname: req.account.firstname,
-        lastname: req.account.lastname
+        lastname: req.account.lastname,
+        likedCount: palettesCount,
+        palettes: palettes,
     });
 });
 
